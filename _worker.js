50445307604220fac12df36387bc1ad24d5ca5b1
appxzm <117379137+appxzm@users.noreply.github.com>
2024-07-26 01:42:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,9 +6,10 @@ async function fetchAndExtractContent(request) {
   const response = await fetch('https://www.cloudflare-cn.com/ips-v4/');
   const text = await response.text();
 
-  // 在当前目录生成 ipv4.txt 文件
-  const fs = require('fs');
-  fs.writeFileSync('ipv4.txt', text);
+  // 使用创建可写流的方式写入文件
+  const writeStream = fs.createWriteStream('ipv4.txt');
+  writeStream.write(text);
+  writeStream.end();
 
   const outputUrl = `${request.url.split('?')[0]}/ip.txt`;  // 生成新的地址
   return new Response(text, {
