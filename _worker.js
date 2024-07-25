72cb73e3bf9796c9a333e29f2a649e338d1dbd5d
appxzm <117379137+appxzm@users.noreply.github.com>
2024-07-26 01:52:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,8 +6,7 @@ async function fetchAndExtractContent(request) {
   const response = await fetch('https://www.cloudflare-cn.com/ips-v4/');
   const text = await response.text();
 
-  // 由于 Cloudflare Pages 环境的限制，无法直接保存文件到本地
-  // 这里可以考虑将获取到的内容以其他方式处理，比如返回给客户端或存储到其他云存储服务
+  // 由于 Cloudflare Pages 的限制，不再进行本地文件写入
 
   const outputUrl = `${request.url.split('?')[0]}/ip.txt`;  // 生成新的地址
   return new Response(text, {
