2675dcf81695863e038bd93e47c3ef17e23d619c
appxzm <117379137+appxzm@users.noreply.github.com>
2024-07-26 01:44:22 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,10 +6,10 @@ async function fetchAndExtractContent(request) {
   const response = await fetch('https://www.cloudflare-cn.com/ips-v4/');
   const text = await response.text();
 
-  // 使用创建可写流的方式写入文件
-  const writeStream = fs.createWriteStream('ipv4.txt');
-  writeStream.write(text);
-  writeStream.end();
+  // 使用 Deno 的标准库来写入文件
+  if (Deno?.writeTextFileSync) {
+    Deno.writeTextFileSync('ipv4.txt', text);
+  }
 
   const outputUrl = `${request.url.split('?')[0]}/ip.txt`;  // 生成新的地址
   return new Response(text, {
